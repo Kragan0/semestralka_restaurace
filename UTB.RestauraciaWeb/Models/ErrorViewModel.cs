315bd72e4d358319893fac6c422fcb06c01073d1
Alex Geleta <alex.geleta11@gmail.com>
2024-10-26 19:05:47 +0200

    Init commit

---
+++ UTB.RestauraciaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UTB.RestauraciaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
